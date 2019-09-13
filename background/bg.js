14c9fb0e61400eb0cd64cfba18b2aefacb486552
tris <a0970699639@gmail.com>
2019-09-13 09:40:54 +0800

    script example

--- background/bg.js
+++ background/bg.js
@@ -1,1 +1,2 @@
 //this is background js 
+console.log("chrome extension background is runing!")
--- content_script/base_script.js
+++ content_script/base_script.js
@@ -1,1 +1,2 @@
 //this is content script js 
+console.log("chrome extension content script is runing!")
